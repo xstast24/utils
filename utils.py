c627da6593a53805a0a260f54183bcd6818295bf
Filip Stastny <filip.stastny@avast.com>
2018-10-12 13:49:27 +0200

    Added universal wait_for

--- utils.py
+++ utils.py
@@ -4,6 +4,26 @@ import re
 import sys
 
 
+def count_lines(path, recursive=True, filters=None):
+    """
+    Count lines in given file/directory. May also search recursively in subdirs etc.
+    Can be filtered, e.g. only certain file types. Note: If no filters given, searching directories may fail,
+    because they may contain some file types (e.g. archives) that can't be read properly as UTF-8 symbols (searching newlines).
+    Note 2: Doesn't count 1 last empty line.
+    :param path: target file or directory
+    :param recursive: recursively count lines also in all files in subdirs, subsubdirs, ..., etc.
+    :param filters: count line only in files matching any of these filters. Filters are regex patterns.
+        Example: [r'\w*.py', r'\w*.sh'] to match *.py and *.sh files
+    :return: number of lines in given file or directory
+    """
+    if os.path.isdir(path):
+        return count_dir_lines(path, recursive, filters)
+    elif os.path.isfile(path):
+        return count_file_lines(path)
+    else:
+        raise FileNotFoundError("Path {} does not exist!".format(path))
+
+
 def count_dir_lines(dir_path, recursive=False, file_filters=None):
     """
     Count lines in all files in given directory. May also search recursively in subdirs etc.
@@ -52,6 +72,30 @@ def count_file_lines(file_path):
     else:
         print("Error: Not a file: {}".format(file_path))
         return -1
+
+
+def wait_for(condition, *args, expected_value=True, timeout=10, period=1, **kwargs):
+    """
+    Wait for condition to have expected value. Condition is checked periodically until timeout is reached.
+    :param condition: variable or callable object that is checked
+    :param args: args for callable condition
+    :param expected_value: desired value of the condition
+    :param timeout: (float) [seconds] max wait time
+    :param period: (float) [seconds] interval to periodically check condition
+    :param kwargs: kwargs for callable condition
+    :return: (bool) True if condition passed under given timeout, otherwise False
+    example: wait_for(math.is_close, my_var, 42, abs_tol=0.01, expected_value=True, timeout=5, period=0.2)
+    """
+    end_time = time.time() + timeout
+    is_callable = callable(condition)
+    while time.time() < end_time:
+        if is_callable and condition(*args, **kwargs) == expected_value:  # condition is a callable -> call the function with given args and check its return value
+            return True
+        elif condition == expected_value:  # is non-callable object -> check value of the object
+            return True
+        time.sleep(period)
+
+    return False
 
 
 """ EXAMPLE USAGE """
